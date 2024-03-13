01b9fd87cf45fab8567b82673bf0f62029ba4a3d
Emil Kelhälä <emil.kelhala@gmail.com>
2024-03-13 07:43:20 +0200

    prepare for submission

--- Assets/Scripts/CargoGoal.cs
+++ Assets/Scripts/CargoGoal.cs
@@ -16,7 +16,7 @@ public class CargoGoal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -5.0f && transform.position.y == 1.2f) {
+        if(transform.position.x < -4.0f && transform.position.y == 1.2f) {
             if(!reached) {
             Vector3 goalPosition = new Vector3(-10.85f, 0.587f, -37.36f);
             rig.transform.position = goalPosition;
